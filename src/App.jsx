57e7772e3d015c743127c839e1ca22d89522e5e8
khubaib shah <bewithbaacha@gmail.com>
2025-04-28 02:20:55 +0500

    Implement chat functionality with state management; connect ChatBotStart and ChatBotApp components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import ChatBotApp from "./components/ChatBotApp";
 import ChatBotStart from "./components/ChatBotStart";
 
 function App() {
+  const [isChatting, setIsChatting] = useState(false);
+  const handleStartChatting = () => {
+    setIsChatting(true);
+  };
+  const handleGoBack = () => {
+    setIsChatting(false);
+  };
   return (
     <>
       <div className="container">
-        {/* <ChatBotStart /> */}
-        <ChatBotApp />
+        {isChatting ? (
+          <ChatBotApp onGoBack={handleGoBack} />
+        ) : (
+          <ChatBotStart onStartChat={handleStartChatting} />
+        )}
       </div>
     </>
   );
